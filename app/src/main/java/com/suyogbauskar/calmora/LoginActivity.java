ef0f235750baf43a69071a429aa527b670479962
shivamdenge <83710729+shivamdenge@users.noreply.github.com>
2025-02-18 23:04:13 +0530

    Created and implemented start and login activity

--- app/src/main/java/com/suyogbauskar/calmora/LoginActivity.java
+++ app/src/main/java/com/suyogbauskar/calmora/LoginActivity.java
@@ -1,8 +1,6 @@
 package com.suyogbauskar.calmora;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class StartActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        loginBtn = findViewById(R.id.login);
-        loginBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(StartActivity.this, LoginActivity.class);
-            startActivity(intent);
         });
     }
 }
