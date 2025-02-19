ec7f34128adc6f586c1720d0a4b4b1f2abc24c9d
shivamdenge <83710729+shivamdenge@users.noreply.github.com>
2025-02-19 13:54:29 +0530

    Create Splash-Screen-Textview-update

---
+++ app/src/main/java/com/suyogbauskar/calmora/SplashScreenActivity.java
@@ -0,0 +1,41 @@
+package com.suyogbauskar.calmora;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    LottieAnimationView animationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        animationView = findViewById(R.id.lottie);
+        animationView.playAnimation();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Intent is used to switch from one activity to another.
+                Intent i = new Intent(SplashScreenActivity.this, StartActivity.class);
+                startActivity(i); // invoke the SecondActivity.
+                finish(); // the current activity will get finished.
+            }
+        }, 3000);
+    }
+}
