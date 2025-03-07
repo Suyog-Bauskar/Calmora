9bab3a07f058211108e7266527bbd4623adc9309
Suyog-Bauskar <suyogbauskar2004@gmail.com>
2025-03-07 17:19:10 +0530

    Add android lint workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.suyogbauskar.calmora"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
