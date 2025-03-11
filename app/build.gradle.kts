ce50be77c4fe2eb9ff9e6ae96e2dfbcdea3a01df
Suyog-Bauskar <64976336+Suyog-Bauskar@users.noreply.github.com>
2025-03-11 16:43:06 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.suyogbauskar.calmora"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
