cb35535e16f05ddb4b02849c5dbbe93ea7e69bcc
shivamdenge <83710729+shivamdenge@users.noreply.github.com>
2025-03-17 23:40:02 +0530

    implement-botton-navigation-bar

--- app/src/main/java/com/suyogbauskar/calmora/HomeActivity.java
+++ app/src/main/java/com/suyogbauskar/calmora/HomeActivity.java
@@ -1,12 +1,16 @@
 package com.suyogbauskar.calmora;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.suyogbauskar.calmora.fragments.HomeFragment;
+import com.suyogbauskar.calmora.fragments.ProfileFragment;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,10 +19,38 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
+
+        Fragment homeFragment = new HomeFragment();
+        Fragment profileFragment = new ProfileFragment();
+
+        // Set default fragment
+        setCurrentFragment(homeFragment);
+
+        // Bottom navigation item selection
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                setCurrentFragment(homeFragment);
+            } else if (itemId == R.id.profile) {
+                setCurrentFragment(profileFragment);
+            }
+            return true;
+        });
+
+    }
+
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, fragment)
+                .commit();
     }
 }
