d6f85c0750374dc2eae958751b1cbfd540b7cbc5
shivamdenge <83710729+shivamdenge@users.noreply.github.com>
2025-03-19 23:09:45 +0530

    Implement-symptom-screen-acrophobia

---
+++ app/src/main/java/com/suyogbauskar/calmora/Adapter/ViewPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.suyogbauskar.calmora.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.suyogbauskar.calmora.fragments.AnxityFragment;
+import com.suyogbauskar.calmora.fragments.DizzinessVertigoFragment;
+import com.suyogbauskar.calmora.fragments.FeelingOfSuffocationFragment;
+import com.suyogbauskar.calmora.fragments.IntenseNeedToEscapeFragment;
+import com.suyogbauskar.calmora.fragments.MentalDistortionFragment;
+import com.suyogbauskar.calmora.fragments.NauseaFragment;
+import com.suyogbauskar.calmora.fragments.ShortnessofBreathFragment;
+import com.suyogbauskar.calmora.fragments.TremblingorShakingFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    public ViewPagerAdapter(@NonNull Fragment fragment) {
+        super(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        switch (position) {
+            case 0:
+                return new DizzinessVertigoFragment();
+            case 1:
+                return new ShortnessofBreathFragment();
+            case 2:
+                return new TremblingorShakingFragment();
+            case 3:
+                return new AnxityFragment();
+            case 4:
+                return new MentalDistortionFragment();
+            case 5:
+                return new IntenseNeedToEscapeFragment();
+            case 6:
+                return new FeelingOfSuffocationFragment();
+            case 7:
+                return new NauseaFragment ();
+            default:
+                return new DizzinessVertigoFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 8;
+    }
+}
--- app/src/main/java/com/suyogbauskar/calmora/fragments/HomeFragment.java
+++ app/src/main/java/com/suyogbauskar/calmora/fragments/HomeFragment.java
@@ -2,65 +2,42 @@ package com.suyogbauskar.calmora.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.suyogbauskar.calmora.Adapter.ViewPagerAdapter;
 import com.suyogbauskar.calmora.R;
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ViewPager2 viewPager;
+    private WormDotsIndicator dotsIndicator;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        viewPager = view.findViewById(R.id.viewPager);
+        dotsIndicator = view.findViewById(R.id.dotsIndicator);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        // Attach TabLayout with ViewPager2
+        dotsIndicator.setViewPager2(viewPager);
+
+
+        return view;
     }
 }
